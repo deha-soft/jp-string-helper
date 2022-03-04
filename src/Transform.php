73941e3033dbe9c432dca7a50431c262d7074762
Bá Ngô <ndba97@gmail.com>
2022-03-04 22:35:02 +0700

    make transform fullsize string to halfsize and reverse

--- src/Transform.php
+++ src/Transform.php
@@ -4,9 +4,27 @@ namespace DehaSoft\JpStringHelper;
 
 trait Transform
 {
-    public function toHalfSize($text)
+    public static function toHalfSize($text)
     {
-        // implement logic
-        return $text;
+        return self::jenKakuToHanKaku(mb_convert_kana($text,"aKCs","UTF-8"));
+    }
+
+    public static function jenKakuToHanKaku($text)
+    {
+        $replace_of = array('ヴ','ガ','ギ','グ','ゲ','ゴ','ザ','ジ','ズ','ゼ','ゾ','ダ','ヂ','ヅ','デ','ド','バ','ビ','ブ','ベ','ボ','パ','ピ','プ','ペ','ポ');
+        $replace_by = array('ｳﾞ','ｶﾞ','ｷﾞ','ｸﾞ','ｹﾞ','ｺﾞ','ｻﾞ','ｼﾞ','ｽﾞ','ｾﾞ','ｿﾞ','ﾀﾞ','ﾁﾞ','ﾂﾞ','ﾃﾞ','ﾄﾞ','ﾊﾞ','ﾋﾞ','ﾌﾞ','ﾍﾞ','ﾎﾞ','ﾊﾟ','ﾋﾟ','ﾌﾟ','ﾍﾟ','ﾎﾟ');
+        return str_replace($replace_of, $replace_by, $text);
+    }
+
+    public static function toFullSize($text)
+    {
+        return self::hanKakuToJenKaku(mb_convert_kana($text,"RA","UTF-8"));
+    }
+
+    public static function hanKakuToJenKaku($text)
+    {
+        $replace_of = array('ｳﾞ','ｶﾞ','ｷﾞ','ｸﾞ','ｹﾞ','ｺﾞ','ｻﾞ','ｼﾞ','ｽﾞ','ｾﾞ','ｿﾞ','ﾀﾞ','ﾁﾞ','ﾂﾞ','ﾃﾞ','ﾄﾞ','ﾊﾞ','ﾋﾞ','ﾌﾞ','ﾍﾞ','ﾎﾞ','ﾊﾟ','ﾋﾟ','ﾌﾟ','ﾍﾟ','ﾎﾟ');
+        $replace_by = array('ヴ','ガ','ギ','グ','ゲ','ゴ','ザ','ジ','ズ','ゼ','ゾ','ダ','ヂ','ヅ','デ','ド','バ','ビ','ブ','ベ','ボ','パ','ピ','プ','ペ','ポ');
+        return str_replace($replace_of, $replace_by, $text);
     }
 }
---
+++ tests/TransformTest.php
@@ -0,0 +1,22 @@
+<?php
+
+use DehaSoft\JpStringHelper\Transform;
+use PHPUnit\Framework\TestCase;
+
+class TransformTest extends TestCase
+{
+    public function testTransformToHalfSize()
+    {
+        $fullSizeStr = "１２３４５ザヂプａｂｃｄ";
+        $output = Transform::toHalfSize($fullSizeStr);
+        $this->assertEquals("12345ｻﾞﾁﾞﾌﾟabcd", $output);
+    }
+
+    public function testTransformToFullSize()
+    {
+        $halfSizeStr = "12345ｻﾞﾁﾞﾌﾟabcd";
+        $output = Transform::toFullSize($halfSizeStr);
+        echo $output;
+        $this->assertEquals("１２３４５ザヂプａｂｃｄ", $output);
+    }
+}
